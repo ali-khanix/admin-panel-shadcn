8a900ac90761029082198786dc69ec3f800fbc8b
Ali Khani <103037526+ali-khanix@users.noreply.github.com>
2025-10-25 01:14:12 +0330

    change theme functionality added to navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="h-screen flex justify-center items-center">
-      <Button>Click Me</Button>
-    </div>
-  );
+  return <div className=""></div>;
 }
